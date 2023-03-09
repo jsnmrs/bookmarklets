d3b881fecf776b17f8286e666023e731b0f32bb6
Jason Morris <jasonmorris@fastmail.com>
2023-03-09 11:40:56 -0500

    Prettier output

--- build.js
+++ build.js
@@ -1,9 +1,11 @@
 import * as esbuild from "esbuild";
 import bookmarkletPlugin from "esbuild-plugin-bookmarklet";
-import {readdirSync} from "fs";
+import { readdirSync } from "fs";
 
 // Using "bookmarklet-" prefix to allow JS reuse
-const bookmarklets = readdirSync("src").filter((file) => file.startsWith("bookmarklet-"));
+const bookmarklets = readdirSync("src").filter((file) =>
+  file.startsWith("bookmarklet-")
+);
 
 for (const bookmarklet of bookmarklets) {
   esbuild.build({
