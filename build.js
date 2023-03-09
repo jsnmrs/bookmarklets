6ad755d7bddad1d283973823bea0a4f548fa1ea4
Jason Morris <jasonmorris@fastmail.com>
2023-03-09 11:28:58 -0500

    Adjust build script

--- build.js
+++ build.js
@@ -1,38 +1,9 @@
 import * as esbuild from "esbuild";
 import bookmarkletPlugin from "esbuild-plugin-bookmarklet";
-
-/* Option A 
-Create an esbuild build for each bookmarklet you want to create
-*/
-
-esbuild.build({
-  entryPoints: ["src/alt-text.js"], // points to normal javascript
-  bundle: true,
-  minify: true,
-  outdir: "dist",
-  write: false,
-  format: "iife",
-  plugins: [bookmarkletPlugin],
-});
-
-esbuild.build({
-  entryPoints: ["src/strip-onpaste.js"], // points to normal javascript
-  bundle: true,
-  minify: true,
-  outdir: "dist",
-  write: false,
-  format: "iife",
-  plugins: [bookmarkletPlugin],
-});
-
-/* Option B - Loop through src folder and automatically create esbuild config for each bookmarklet
-
 import {readdirSync} from "fs";
 
-// You may need to create a repeatable pattern for each bookmarket's entrypoint file
-// For example `strip-onpaste.js` could be `bookmarklet-strip-onpaste.js` and then update the filter below to check that the file begins with `bookmarklet-`
-// Otherwise this will bundle the "alt-text-node.js" file into it's only file as well
-const bookmarklets = readdirSync("src").filter((file) => file.endsWith(".js"));
+// Using "bookmarklet-" prefix to allow JS reuse
+const bookmarklets = readdirSync("src").filter((file) => file.startsWith("bookmarklet-"));
 
 for (const bookmarklet of bookmarklets) {
   esbuild.build({
@@ -45,4 +16,3 @@ for (const bookmarklet of bookmarklets) {
     plugins: [bookmarkletPlugin],
   });
 }
-*/
