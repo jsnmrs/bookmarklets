348633c67ebc722fcd80b7c14f4d466e18c67fbd
Jason Morris <jasonmorris@fastmail.com>
2023-03-22 21:22:18 -0400

    Split up esbuild and rest of data transformation

--- build.js
+++ build.js
@@ -1,5 +1,3 @@
-import * as esbuild from "esbuild";
-import bookmarkletPlugin from "esbuild-plugin-bookmarklet";
 import * as fs from "fs";
 
 const referenceData = fs.readFileSync("data/references.json"),
@@ -7,21 +5,7 @@ const referenceData = fs.readFileSync("data/references.json"),
   dist = "./dist/";
 let data = [];
 
-if (!fs.existsSync(dist)) {
-  fs.mkdirSync(dist);
-}
-
 references.forEach((reference) => {
-  esbuild.build({
-    entryPoints: ["bookmarklets/" + reference.file],
-    bundle: true,
-    minify: true,
-    outdir: "dist",
-    write: false,
-    format: "iife",
-    plugins: [bookmarkletPlugin],
-  });
-
   let distPath = dist + reference.file;
   const code = fs.readFileSync(distPath, "utf-8");
 
