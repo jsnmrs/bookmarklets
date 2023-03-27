2086b679f6ce4b9e9547c9d6451897d95b6e323c
Jason Morris <jasonmorris@fastmail.com>
2023-03-26 20:31:13 -0400

    Add four more

---
+++ bookmarklets/ct.js
@@ -0,0 +1,7 @@
+(function () {
+  var ct = document.createElement("link");
+  ct.rel = "stylesheet";
+  ct.href = "https://csswizardry.com/ct/ct.css";
+  ct.classList.add("ct");
+  document.head.appendChild(ct);
+})();
---
+++ bookmarklets/tota11y2.js
@@ -0,0 +1,6 @@
+(function () {
+  var tota11y = document.createElement("SCRIPT");
+  tota11y.type = "text/javascript";
+  tota11y.src = "https://tota11y.babylontech.co.uk/tota11y.min.js";
+  document.getElementsByTagName("head")[0].appendChild(tota11y);
+})();
