c122a5cc5a968c4477b5d65d4cae9e240aca80c6
Jason Morris <jasonmorris@fastmail.com>
2023-03-28 21:46:28 -0400

    Improve CSP test

--- bookmarklets/test-csp.js
+++ bookmarklets/test-csp.js
@@ -1,17 +1,14 @@
 document.addEventListener("securitypolicyviolation", cspCheck, false);
 
+var testScript = document.createElement("script");
+testScript.setAttribute(
+  "src",
+  "https://jsnmrs.github.io/bookmarklets/bookmarklets/test-js-external.js"
+);
+document.body.appendChild(testScript);
+
 function cspCheck(e) {
   "use strict";
-  alert(e.blockedURI);
-  alert(e.violatedDirective);
-  alert(e.originalPolicy);
+
+  alert(e.violatedDirective + " breaks Content Security Policy (CSP)");
 }
-
-var style = document.createElement("style"),
-  styleContent = document.createTextNode(
-    'body::after{ position: absolute; top: 0; right: 4px; background-color: #000; color: #01ff70; z-index: 9999; font-size: 16px; font-weight: 400; padding: 3px 9px; outline: 4px solid #01ff70 !important; content: "checking for CSP";}}'
-  ),
-  head = document.getElementsByTagName("head");
-
-style.appendChild(styleContent);
-head[0].appendChild(style);
