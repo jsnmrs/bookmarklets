9ad0b4bdb259e5910d61ba053420ae15e4eca56c
Jason Morris <jasonmorris@fastmail.com>
2023-03-28 21:25:43 -0400

    Display ID in a prompt for quick copy/paste

--- bookmarklets/youtube-uc-id.js
+++ bookmarklets/youtube-uc-id.js
@@ -1,15 +1,8 @@
 var result = document
-    .querySelector("link[rel=canonical]")
-    .getAttribute("href")
-    .replace("https://www.youtube.com/channel/", ""),
-  output = document.createElement("p"),
-  page = document.getElementsByTagName("body");
+  .querySelector("link[rel=canonical]")
+  .getAttribute("href")
+  .replace("https://www.youtube.com/channel/", "");
 
 if (result) {
-  output.textContent = result;
-  output.setAttribute(
-    "style",
-    "position: absolute; top: 0; right: 4px; background-color: #000; color: #01ff70; z-index: 9999; font-size: 16px; font-weight: 400; padding: 3px 9px; outline: 4px solid #01ff70 !important;"
-  );
-  page[0].prepend(output);
+  prompt("UC ID:", result);
 }
