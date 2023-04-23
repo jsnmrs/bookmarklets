1f51d10a2a83accc4f4679fb190cd92bd263e858
Jason Morris <jasonmorris@fastmail.com>
2023-04-23 10:18:14 -0400

    Autocomplete, HTML CodeSniffer

---
+++ bookmarklets/autocomplete.js
@@ -0,0 +1,8 @@
+(function () {
+  var element = document.createElement("script");
+  element.setAttribute(
+    "src",
+    "https://mraccess77.github.io/favlets/autocomplete_attribute.js"
+  );
+  document.body.appendChild(element);
+})();
