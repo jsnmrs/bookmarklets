2c212b13259ae0bbcc5a540061cad0a563518fd9
Jason Morris <jasonmorris@fastmail.com>
2023-04-28 16:16:43 -0400

    Normalize name and approach

--- bookmarklets/run-lighthouse.js
+++ bookmarklets/run-lighthouse.js
@@ -1,4 +1,5 @@
-void (document.location =
+void window.open(
   "https://googlechrome.github.io/lighthouse/viewer/?psiurl=" +
-  escape(document.location) +
-  "&strategy=mobile&category=performance&category=accessibility&category=best-practices&category=seo&category=pwa");
+    escape(window.location) +
+    "&strategy=mobile&category=performance&category=accessibility&category=best-practices&category=seo&category=pwa"
+);
