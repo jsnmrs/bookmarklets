7a2844073b8226e5bb01830bc5d515a19c7a8a26
Jason Morris <jasonmorris@fastmail.com>
2023-04-28 16:59:53 -0400

    Localize

--- bookmarklets/duplicate-ids.js
+++ bookmarklets/duplicate-ids.js
@@ -1,8 +1,27 @@
-void (function () {
-  var element = document.createElement("script");
-  element.setAttribute(
-    "src",
-    "https://mraccess77.github.io/favlets/duplicateIds.js"
-  );
-  document.body.appendChild(element);
-})();
+function runDuplicateIds() {
+	var clone = document.cloneNode(true);
+	var col = clone.querySelectorAll("[id]");
+	//alert(find_duplicate_ids(Array.prototype.slice.call( col, 0 )).length);
+	//
+	var results=[];
+	find_duplicate_ids(col, results);
+	var strResults = results.length+" elements with duplicate ids"+"\n";
+
+	results.forEach(function(item) {
+		 //item.style.border = "thin solid magenta";
+		 item.innerHTML ="";
+		 strResults = strResults + item.outerHTML + "\n"
+	});
+	alert(strResults);
+}
+
+function find_duplicate_ids(arIDs, results) {
+	 arIDs.forEach(function (item) {
+			ids = document.querySelectorAll('[id="'+item.id+'"]');
+			 if (ids.length>1)
+					results.push(item);
+	 });
+}
+runDuplicateIds();
+
+
