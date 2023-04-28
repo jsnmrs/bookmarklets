03904be7a941793bd683132b3908e1c2e89c5832
Jason Morris <jasonmorris@fastmail.com>
2023-04-28 17:07:09 -0400

    Localize

--- bookmarklets/duplicate-ids.js
+++ bookmarklets/duplicate-ids.js
@@ -1,27 +1,24 @@
 function runDuplicateIds() {
-	var clone = document.cloneNode(true);
-	var col = clone.querySelectorAll("[id]");
-	//alert(find_duplicate_ids(Array.prototype.slice.call( col, 0 )).length);
-	//
-	var results=[];
-	find_duplicate_ids(col, results);
-	var strResults = results.length+" elements with duplicate ids"+"\n";
+  var clone = document.cloneNode(true);
+  var col = clone.querySelectorAll("[id]");
+  //alert(find_duplicate_ids(Array.prototype.slice.call( col, 0 )).length);
+  //
+  var results = [];
+  find_duplicate_ids(col, results);
+  var strResults = results.length + " elements with duplicate ids" + "\n";
 
-	results.forEach(function(item) {
-		 //item.style.border = "thin solid magenta";
-		 item.innerHTML ="";
-		 strResults = strResults + item.outerHTML + "\n"
-	});
-	alert(strResults);
+  results.forEach(function (item) {
+    //item.style.border = "thin solid magenta";
+    item.innerHTML = "";
+    strResults = strResults + item.outerHTML + "\n";
+  });
+  alert(strResults);
 }
 
 function find_duplicate_ids(arIDs, results) {
-	 arIDs.forEach(function (item) {
-			ids = document.querySelectorAll('[id="'+item.id+'"]');
-			 if (ids.length>1)
-					results.push(item);
-	 });
+  arIDs.forEach(function (item) {
+    var ids = document.querySelectorAll('[id="' + item.id + '"]');
+    if (ids.length > 1) results.push(item);
+  });
 }
 runDuplicateIds();
-
-
