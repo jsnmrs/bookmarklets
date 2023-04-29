2c822eced9d655ea6948eaa57d942d0e43184127
Jason Morris <jasonmorris@fastmail.com>
2023-04-28 20:43:26 -0400

    Localize force focus

--- bookmarklets/force-focus.js
+++ bookmarklets/force-focus.js
@@ -1,11 +1,8 @@
-(function () {
-  document.body.appendChild(document.createElement("script")).src =
-    "https://cdn.jsdelivr.net/gh/pauljadam/bookmarklets@master/focus.js";
-  var iframes = document.getElementsByTagName("iframe");
-  for (var i = 0; i < iframes.length; i++) {
-    iframes[i].contentDocument.body.appendChild(
-      document.createElement("script")
-    ).src =
-      "https://cdn.jsdelivr.net/gh/pauljadam/bookmarklets@master/focus.js";
-  }
-})();
+var style = document.createElement("style"),
+  styleContent = document.createTextNode(
+    "a:focus, *:focus { outline: 4px solid orange !important; outline-offset:1px !important; }"
+  ),
+  head = document.getElementsByTagName("head");
+
+style.appendChild(styleContent);
+head[0].appendChild(style);
