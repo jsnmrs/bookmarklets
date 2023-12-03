28654f0020d677c75106f56729a04b24b71e9e35
Jason Morris <jasonmorris@fastmail.com>
2023-12-03 18:42:55 -0500

    Remove bookmarklet, source removed

--- bookmarklets/tool-gutoaf.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var element = document.createElement("script");
-  element.setAttribute(
-    "src",
-    "https://labs.levelaccess.com/images/4/43/GUToAF.js"
-  );
-  document.body.appendChild(element);
-})();
