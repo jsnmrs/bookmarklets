2373ff5e84429a96cdfdf430fccce739ff62bd12
Jason Morris <jasonmorris@fastmail.com>
2023-12-11 21:07:12 -0500

    Add body outline, alter if scroll detected

--- bookmarklets/horizontal-scroll.js
+++ bookmarklets/horizontal-scroll.js
@@ -1,7 +1,8 @@
 (function () {
+  document.body.style.outline = "5px solid green";
   window.addEventListener("resize", function () {
     if (document.body.scrollWidth > window.innerWidth) {
-      alert("Horizontal scrollbar detected!");
+      document.body.style.outline = "10px dashed red";
     }
   });
 })();
