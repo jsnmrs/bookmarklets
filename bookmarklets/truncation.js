de5ead8d7ab60fae2b133d42ae0c150ebf276260
Jason Morris <jasonmorris@fastmail.com>
2023-12-11 20:46:29 -0500

    Add `text-overflow: ellipsis` check and demo CSS

--- bookmarklets/truncation.js
+++ bookmarklets/truncation.js
@@ -1,65 +1,34 @@
 (function () {
-  var ellipsisFound = false;
-
-  // Helper function to check for "text-overflow: ellipsis" and outline the element
-  function checkAndOutline(element) {
-    if (element.style.cssText.indexOf("text-overflow: ellipsis") !== -1) {
-      ellipsisFound = true;
-      element.style.outline = "red solid 2px";
-    }
-  }
-
-  // Check stylesheets for "text-overflow: ellipsis"
-  var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
-  for (var i = 0; i < stylesheets.length; i++) {
-    var sheet = stylesheets[i];
-    var rules = sheet.sheet.cssRules;
-
-    for (var j = 0; j < rules.length; j++) {
-      var rule = rules[j];
-      if (rule.selectorText) {
+  function checkRules(sheet) {
+    var rules = sheet.cssRules || sheet.rules;
+    for (var i = 0; i < rules.length; i++) {
+      var rule = rules[i];
+      if (rule.style && rule.style.textOverflow === "ellipsis") {
         var elements = document.querySelectorAll(rule.selectorText);
-        for (var k = 0; k < elements.length; k++) {
-          checkAndOutline(elements[k]);
-        }
+        elements.forEach(function (element) {
+          element.style.border = "5px solid red";
+        });
       }
     }
   }
 
-  // Check style tags for "text-overflow: ellipsis"
-  var styleTags = document.querySelectorAll("style");
-  for (i = 0; i < styleTags.length; i++) {
-    var styleText = styleTags[i].textContent;
-    rules = styleText.match(/([^{}]+)\s*\{(.*?)\}\s*/g);
+  var styles = document.querySelectorAll("style");
+  styles.forEach(function (style) {
+    checkRules(style.sheet);
+  });
 
-    if (rules) {
-      for (j = 0; j < rules.length; j++) {
-        rule = rules[j].split("{");
-        var selector = rule[0].trim();
-        var styles = rule[1].trim();
-
-        if (selector && styles) {
-          elements = document.querySelectorAll(selector);
-          for (k = 0; k < elements.length; k++) {
-            if (styles.indexOf("text-overflow:ellipsis") !== -1) {
-              checkAndOutline(elements[k]);
-            }
-          }
-        }
-      }
+  var links = document.querySelectorAll('link[rel="stylesheet"]');
+  links.forEach(function (link) {
+    var sheet = link.sheet;
+    if (sheet) {
+      checkRules(sheet);
     }
-  }
+  });
 
-  // Check inline styles for "text-overflow: ellipsis"
-  elements = document.querySelectorAll("*");
-  for (i = 0; i < elements.length; i++) {
-    checkAndOutline(elements[i]);
-  }
-
-  // Show an alert if "text-overflow: ellipsis" was found
-  if (ellipsisFound) {
-    alert(
-      'This page uses "text-overflow: ellipsis" on some elements. Outlined in red.'
-    );
-  }
+  var elements = document.querySelectorAll("*");
+  elements.forEach(function (element) {
+    if (element.style.textOverflow === "ellipsis") {
+      element.style.border = "5px solid red";
+    }
+  });
 })();
